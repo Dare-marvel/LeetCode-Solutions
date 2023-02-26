e7e2b2c57829c9f21d5ddc3fc5231166b566145f
Adwait S Purao <104304028+Dare-marvel@users.noreply.github.com>
2023-02-26 15:46:51 +0530

    Update ReverseInteger.py

--- PYTHON/ReverseInteger.py
+++ PYTHON/ReverseInteger.py
@@ -1,12 +1,26 @@
+# Link to the problem : https://leetcode.com/problems/reverse-integer/
+
 class Solution:
     def reverse(self, x: int) -> int:
-        num,rev_num = abs(x),0
-        while num!=0:
-            rev_num = rev_num*10 + num%10
-            num=num//10
-        if rev_num<-2**31 or rev_num>2**31 - 1:
+        # Extract the absolute value of the input number.
+        num = abs(x)
+        # Initialize the reversed number to zero.
+        rev_num = 0
+        
+        # Reverse the digits of the input number.
+        while num != 0:
+            # Multiply the current reversed number by 10 and add the next digit of the input number.
+            rev_num = rev_num * 10 + num % 10
+            # Move to the next digit of the input number.
+            num = num // 10
+        
+        # Check if the reversed number is outside the range of a 32-bit signed integer.
+        if rev_num < -2**31 or rev_num > 2**31 - 1:
             return 0
-        elif x>0:
+        # If the input number was positive, return the reversed number.
+        elif x > 0:
             return rev_num
-        else :
-            return rev_num*(-1)
+        # Otherwise, return the negated reversed number.
+        else:
+            return rev_num * (-1)
+
