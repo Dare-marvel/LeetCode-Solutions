a73a8665e05cd5ebb03b3a79ee23fcf33db643cd
Adwait S Purao <104304028+Dare-marvel@users.noreply.github.com>
2023-04-01 09:24:28 +0530

    Update TwoSum.py

--- PYTHON/TwoSum.py
+++ PYTHON/TwoSum.py
@@ -1,8 +1,35 @@
 # Link : https://leetcode.com/problems/two-sum/description/
 
 # Key Insight :
+# use a dictionary to store the indices of each element in the input list,
+# and then iterate over the list to check if the complement of each element is already in the dictionary . If the complement is found 
+# return the indices 
+
+# Optimal Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Create an empty dictionary to store the indices of each element in the input list
+        index_dict = {}
+        
+        # Iterate over the input list using the enumerate() function to keep track of the indices
+        for i , num in enumerate(nums):
+            # Compute the complement of the current element (i.e., the value that is needed to reach the target sum)
+            complement = target - num
+            
+            # Check if the complement is already in the dictionary. If it is, we have found a pair of elements that add up to the target sum.
+            # Return their indices as a list and exit the loop.
+            if complement in index_dict:
+                return [index_dict[complement],i]
+            
+            # If the complement is not in the dictionary, add the current element and its index to the dictionary and continue iterating.
+            index_dict[num] = i
+        
+        # If no pair is found, return an empty list.
+        return []
+# ------------------------------------------------------------------------------------------------------------------------------------------- 
 
 
+# Working but not a optimal solution
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # Sort the input list of numbers
